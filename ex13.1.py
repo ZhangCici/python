23969afba52732d3ef715116009f05edbae4931d
张太阳 <m18626927365@163.com>
2017-08-03 23:38:09 +0800

    Add files via upload

---
+++ ex13.1.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, first, second = argv
+
+print "This is ", script
+print "My first name is ", first
+print "My second name is ", second
---
+++ ex13.2.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: ", script
+print "So what do you want to say? ", first
+print "I think ", second
+print "I can tell you that ", third
+
+x = raw_input("What eles?")
